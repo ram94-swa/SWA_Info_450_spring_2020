96fa17059d55c7bf3c172c2e41839521c75e8a95
ram94-swa <59984376+ram94-swa@users.noreply.github.com>
2020-02-13 18:26:45 -0500

    commit

---
+++ week4/program2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int Names=30; int Char=20;
+    int i,j;
+    char students[Names][Char];
+
+//for(i=0;i<Names;i++)
+{
+    for(j=0;j<Char;j++){
+    cout<<"please enter student for index" << i <<":"<<endl; 
+}
+for(i=0;i<Names;i++)
+{
+    for(j=0;j<Char;j++){
+    cout<<students[Names][Char]<<endl;
+        }
+    }
+}
+return 0;
+}
